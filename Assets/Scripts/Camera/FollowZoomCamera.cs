261347fe7f4ca7350f18c53ccc722ee8c3d68554
abyssilver <96397692+abyssilver@users.noreply.github.com>
2025-05-02 20:21:45 +0900

    카메라 이동, 줌 스크립트 병합

--- Assets/Scripts/Camera/FollowZoomCamera.cs
+++ Assets/Scripts/Camera/FollowZoomCamera.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZoomCamera : MonoBehaviour
+public class FollowZoomCamera : MonoBehaviour
 {
 
-    public float zoomSpeed = 2f;
+    public Transform target;
+    float offsetX;
+    float offsetY;
 
+    public float zoomSpeed = 2f;
     public float minSize = 3f;
     public float maxSize = 9f;
 
@@ -16,17 +19,32 @@ public class ZoomCamera : MonoBehaviour
     void Start()
     {
         _camera = Camera.main;
+
+        if (target != null)
+        {
+            offsetX = transform.position.x - target.position.x;
+            offsetY = transform.position.y - target.position.y;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+
         float scroll = Input.GetAxis("Mouse ScrollWheel");
 
         if (scroll != 0f)
         {
             _camera.orthographicSize -= scroll * zoomSpeed;
             _camera.orthographicSize = Mathf.Clamp(_camera.orthographicSize, minSize, maxSize);
+        }
+
+        if (target != null)
+        {
+            Vector3 pos = transform.position;
+            pos.x = target.position.x + offsetX;
+            pos.y = target.position.y + offsetY;
+            transform.position = pos;
         }
     }
 }
