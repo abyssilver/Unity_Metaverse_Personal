520a1512c0c85cfb8b0133dc4e620891d64edde4
abyssilver <96397692+abyssilver@users.noreply.github.com>
2025-05-07 14:56:12 +0900

    게임ui에 시간출력 ..(까먹은거 추가함..)

--- Assets/Scripts/UI/GameUI.cs
+++ Assets/Scripts/UI/GameUI.cs
@@ -8,12 +8,20 @@ public class GameUI : BaseUI
 {
     [SerializeField] public TextMeshProUGUI waveText;
     [SerializeField] public Slider hpSlider;
+    [SerializeField] public TextMeshProUGUI timeText;
+
+    private float elapsedTime;
 
     private void Start()
     {
         UpdateHPSlider(1);
     }
 
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        timeText.text = elapsedTime.ToString("F2");
+    }
     public void UpdateHPSlider(float percentage)
     {
         hpSlider.value = percentage;
