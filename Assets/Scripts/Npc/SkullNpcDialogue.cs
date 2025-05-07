6de901d253fd366997b18a5afe4685d4779cdcad
abyssilver <96397692+abyssilver@users.noreply.github.com>
2025-05-07 14:12:56 +0900

    단순 수치조절

--- Assets/Scripts/Npc/SkullNpcDialogue.cs
+++ Assets/Scripts/Npc/SkullNpcDialogue.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -22,12 +23,22 @@ public class SkullNpcDialogue : MonoBehaviour
         if (!other.CompareTag("Player"))
             return;
 
-        //int clearedWave = 0;
+        if (ScoreManager.Instance.LastClearWave == 0)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"오늘은 아직 미니게임을 진행하지 않았네.");
+            sb.AppendLine($"미니게임을 진행하려면 방안의 분수를 살펴봐!");
+            dialogueText.text = sb.ToString();
+        }
+        else
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"기록을 보고싶으면 입구 양옆의 배너를 확인해봐!");
+            sb.AppendLine($"다시 게임을하고 싶으면 분수대를 다시 조사해봐!");
+            dialogueText.text = sb.ToString();
+        }
 
-        //if (MinigameManager.instance != null)
-        //    clearedWave = MinigameManager.instance.lastClearedWave;
-
-        dialogueText.text = $"미니게임을 진행하려면 방안의 분수를 살펴봐!";
+        
         dialogueUI.SetActive(true);
     }
 
